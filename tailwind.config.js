e51c9232637ec71de25396b08afd9af446c8928a
zidanabbas <zidan.abbas28@gmail.com>
2024-06-12 17:29:33 +0700

    add configuration padding container, layout, screens and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,19 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      screens: {
+        "2xl": "1320px",
+      },
+      colors: {
+        primary: "#FC8F00",
+        secondary: "#BE710B",
+        third: "#F9BC60",
+        "background-color": "#FFFFFE",
       },
     },
   },
