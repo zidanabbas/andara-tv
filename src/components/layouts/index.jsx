e560322b6f459605331a9518ba04b7ebba9283b8
zidanabbas <zidan.abbas28@gmail.com>
2024-06-19 00:02:46 +0700

    style : Add height to root layouts

--- src/components/layouts/index.jsx
+++ src/components/layouts/index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 export default function Layouts({ children }) {
   return (
-    <div className="w-full bg-background-color overflow-hidden">{children}</div>
+    <div className="w-full h-full bg-background-color overflow-hidden">
+      {children}
+    </div>
   );
 }
