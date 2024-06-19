bf71f218ba959729e065ac8fda0d274e37ae7fe1
zidanabbas <zidan.abbas28@gmail.com>
2024-06-19 16:10:43 +0700

    feat : create dummy data for the gallery

---
+++ src/lib/utils/DataGallery.js
@@ -0,0 +1,28 @@
+const DataGallery = [
+  {
+    id: 1,
+    url: "https://res.cloudinary.com/dlshk9mf6/image/upload/v1718786765/AndaraTV/brpqukvj8zieye7eqjbf.jpg",
+  },
+  {
+    id: 2,
+    url: "https://res.cloudinary.com/dlshk9mf6/image/upload/v1718786768/AndaraTV/opxllhq4zrytqeotvtic.jpg",
+  },
+  {
+    id: 3,
+    url: "https://res.cloudinary.com/dlshk9mf6/image/upload/v1718786776/AndaraTV/xopzz8xmxl4iv5ta6gxt.jpg",
+  },
+  {
+    id: 4,
+    url: "https://res.cloudinary.com/dlshk9mf6/image/upload/v1718786763/AndaraTV/ycezygxz56fu9etloysn.jpg",
+  },
+  {
+    id: 5,
+    url: "https://res.cloudinary.com/dlshk9mf6/image/upload/v1718786763/AndaraTV/f0ydl2aits5nxbncaqdu.jpg",
+  },
+  {
+    id: 6,
+    url: "https://res.cloudinary.com/dlshk9mf6/image/upload/v1718786757/AndaraTV/dmsbtpxvb2dxqywu8v1f.jpg",
+  },
+];
+
+export default DataGallery;
