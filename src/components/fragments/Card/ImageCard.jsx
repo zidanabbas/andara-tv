fd75045513cf2535f31de314ea4e71dbf4aab367
zidanabbas <zidan.abbas28@gmail.com>
2024-06-19 16:15:03 +0700

    refactor : making props for picture cards

--- src/components/fragments/Card/ImageCard.jsx
+++ src/components/fragments/Card/ImageCard.jsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
 import React from "react";
 
-export default function ImageCard() {
+export default function ImageCard({ src, alt }) {
   return (
-    <div className="max-w-64 max-h-64">
+    <div className="max-w-64 max-h-64 p-2 bg-white rounded-lg">
       <Image
-        src={"/images/ilustrator.png"}
-        alt="ilustrator"
+        src={src}
+        alt={alt}
         width={500}
         height={500}
-        className="w-auto h-auto bg-white rounded-lg"
+        className="w-auto h-auto rounded-lg hover:scale-105 ease-in-out duration-500"
       />
     </div>
   );
