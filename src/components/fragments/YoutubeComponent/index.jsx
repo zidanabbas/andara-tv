cbaf2c1d09f67e579e5a4994eed5fff43d49bf54
zidanabbas <zidan.abbas28@gmail.com>
2024-06-20 02:39:30 +0700

    refactor : refactor youtube component code

--- src/components/fragments/YoutubeComponent/index.jsx
+++ src/components/fragments/YoutubeComponent/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-export default function YoutubeComponent() {
+export default function YoutubeComponent({ title, embed }) {
   return (
     <div className="max-w-sm max-h-72 flex flex-col items-center rounded-lg p-4 relative shadow-xl shadow-slate-600">
       <iframe
-        src="https://www.youtube.com/embed/sHsZt44Mg4w?si=KekyluQBsjgq4ijo"
-        title="YouTube video player"
+        src={`https://www.youtube.com/embed/${embed}`}
+        // src={src}
+        title={title}
         width={200}
         height={200}
         frameBorder="0"
