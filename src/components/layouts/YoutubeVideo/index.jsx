d0df806002039009813ac88d2953b7f9d371caa9
zidanabbas <zidan.abbas28@gmail.com>
2024-06-20 16:02:51 +0700

    style : added a title for the last video uploaded and added validation for loading

--- src/components/layouts/YoutubeVideo/index.jsx
+++ src/components/layouts/YoutubeVideo/index.jsx
@@ -5,34 +5,44 @@ import Link from "next/link";
 import YoutubeComponent from "@/components/fragments/YoutubeComponent/index.jsx";
 import SectionContainerBody from "../SectionPage/SectionContainerBody.jsx";
 import { fetchVideos } from "@/services/youtube-api.js";
+import ContainTitle from "@/components/ui/ContainTitle.jsx";
 
 export default function YoutubeVideo() {
-  const [latestVideos, setLatestVideos] = useState([]);
+  const [LastVideos, setLastVideos] = useState([]);
   useEffect(() => {
     const fetchYoutubeVideo = async () => {
       await fetchVideos((items) => {
-        setLatestVideos(items);
+        setLastVideos(items);
       });
     };
 
     fetchYoutubeVideo();
   }, []);
 
-  // console.log(latestVideos);
   return (
     <ContainerBody classname={"pt-28 pb-28 px-4 md:px-10"}>
       <SectionContainerBody
         title={"Youtube Video"}
         classname={"border border-slate-300 lg:max-h-[700px]"}
       >
-        <div className="rounded-xl bg-slate-100 border border-slate-500 flex flex-wrap justify-center gap-3 px-4 py-5 max-h-[600px] md:max-h-[700px] lg:max-h-fit overflow-x-auto">
-          {latestVideos.map((item) => (
-            <YoutubeComponent
-              key={item.id.videoId}
-              embed={item.id.videoId}
-              title={item.snippet.title}
-            />
-          ))}
+        <ContainTitle
+          title={"video paling terbaru"}
+          classname={"font-semibold text-black"}
+        />
+        <div className="w-full rounded-xl bg-slate-100 border border-slate-500 flex flex-wrap justify-center gap-3 px-4 py-5 max-h-[600px] md:max-h-[500px] overflow-x-auto">
+          {LastVideos.length > 0 ? (
+            LastVideos.slice(1).map((item) => (
+              <YoutubeComponent
+                key={item.id.videoId}
+                embed={item.id.videoId}
+                title={item.snippet.title}
+              />
+            ))
+          ) : (
+            <h1 className="text-base font-medium">
+              Tunggu sebentar ye cang 😁 ....
+            </h1>
+          )}
         </div>
         <Link href="/profile" className="flex justify-center">
           <button className="bg-primary text-white rounded-lg p-2">
