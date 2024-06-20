f9c4da84f6d743499897fa44095cf3c70f6e72f4
zidanabbas <zidan.abbas28@gmail.com>
2024-06-20 16:03:29 +0700

    style : added props for classname

--- src/components/ui/ContainTitle.jsx
+++ src/components/ui/ContainTitle.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-export default function ContainTitle({ title }) {
+export default function ContainTitle({ title, classname }) {
   return (
     <div className="w-full px-4">
-      <h2 className="text-base max-w-48 font-bold text-primary tracking-widest">
+      <h2
+        className={`text-base font-bold text-primary tracking-widest ${classname}`}
+      >
         {title}
       </h2>
     </div>
