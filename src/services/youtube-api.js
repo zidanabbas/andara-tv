e29bf88300953dfb32864881ee4488a7ac1a3629
zidanabbas <zidan.abbas28@gmail.com>
2024-06-20 16:00:16 +0700

    fix : create api validation if it doesn't succeed in getting the data

--- src/services/youtube-api.js
+++ src/services/youtube-api.js
@@ -3,6 +3,9 @@ const endpointURL = process.env.NEXT_PUBLIC_LATEST_VIDEO;
 export const fetchVideos = async (callback) => {
   try {
     const response = await fetch(`${endpointURL}`);
+    if (response.status !== 200) {
+      throw new Error("Failed to fetch videos");
+    }
     const youtubeData = await response.json();
     callback(youtubeData.items);
     // console.log(data);
