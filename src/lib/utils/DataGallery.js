1d4a81da33ffc709a5b21911474313a8d26e5759
zidanabbas <zidan.abbas28@gmail.com>
2024-06-20 16:00:59 +0700

    Add title data

--- src/lib/utils/DataGallery.js
+++ src/lib/utils/DataGallery.js
@@ -1,26 +1,32 @@
 const DataGallery = [
   {
     id: 1,
+    title: "Andara TV",
     url: "https://res.cloudinary.com/dlshk9mf6/image/upload/v1718786765/AndaraTV/brpqukvj8zieye7eqjbf.jpg",
   },
   {
     id: 2,
+    title: "Andara TV",
     url: "https://res.cloudinary.com/dlshk9mf6/image/upload/v1718786768/AndaraTV/opxllhq4zrytqeotvtic.jpg",
   },
   {
     id: 3,
+    title: "Andara TV",
     url: "https://res.cloudinary.com/dlshk9mf6/image/upload/v1718786776/AndaraTV/xopzz8xmxl4iv5ta6gxt.jpg",
   },
   {
     id: 4,
+    title: "Andara TV",
     url: "https://res.cloudinary.com/dlshk9mf6/image/upload/v1718786763/AndaraTV/ycezygxz56fu9etloysn.jpg",
   },
   {
     id: 5,
+    title: "Andara TV",
     url: "https://res.cloudinary.com/dlshk9mf6/image/upload/v1718786763/AndaraTV/f0ydl2aits5nxbncaqdu.jpg",
   },
   {
     id: 6,
+    title: "Andara TV",
     url: "https://res.cloudinary.com/dlshk9mf6/image/upload/v1718786757/AndaraTV/dmsbtpxvb2dxqywu8v1f.jpg",
   },
 ];
