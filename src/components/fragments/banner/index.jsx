f1864e0446dbbf10f0f4fa4ad83efec5fa2aff26
zidanabbas <zidan.abbas28@gmail.com>
2024-07-01 01:35:13 +0700

    refactor : Refactor Banner component styles - Removed unnecessary 'mx-auto' and 'py-28' classes for improved layout flexibility

--- src/components/fragments/banner/index.jsx
+++ src/components/fragments/banner/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Banner({ classname, children }) {
   return (
     <div
-      className={`w-full mx-auto py-28 bg-cover bg-center ${classname}`}
+      className={`w-full bg-cover bg-center ${classname}`}
       style={{ backgroundImage: `url('/images/banner.png')` }}
     >
       {children}
