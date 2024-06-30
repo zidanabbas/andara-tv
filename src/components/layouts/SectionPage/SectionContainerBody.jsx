984d6a4a743c90ec9da3c8de22670508f4707b42
zidanabbas <zidan.abbas28@gmail.com>
2024-07-01 01:39:31 +0700

    style : Simplify SectionContainerBody component structure - Streamlined the component structure by removing 'title' prop and ContainTitle component dependency - Adjusted styles for improved consistency and readability

--- src/components/layouts/SectionPage/SectionContainerBody.jsx
+++ src/components/layouts/SectionPage/SectionContainerBody.jsx
@@ -1,12 +1,8 @@
-import ContainTitle from "@/components/ui/ContainTitle";
 import React from "react";
 
-export default function SectionContainerBody({ classname, title, children }) {
+export default function SectionContainerBody({ classname, children }) {
   return (
-    <div
-      className={`container rounded-lg flex flex-col gap-4 px-6 py-6 justify-center items-center max-h-[500px] sm:max-h-[600px] md:max-h-[750px] lg:max-h-fit ${classname}`}
-    >
-      <ContainTitle title={title} />
+    <div className={`container rounded-lg gap-6 px-6 py-6 ${classname}`}>
       {children}
     </div>
   );
