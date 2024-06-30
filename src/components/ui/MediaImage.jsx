f9205035e007c5417d2a2c7c4a267b5eca4b26e3
zidanabbas <zidan.abbas28@gmail.com>
2024-07-01 01:53:17 +0700

    feat : Add image media components

---
+++ src/components/ui/MediaImage.jsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import React from "react";
+
+export default function MediaImage() {
+  return (
+    <div className="sm:w-64 md:w-80 lg:w-72 mb-4 sm:mb-0 shadow-lg">
+      <Image
+        src={"/images/ilustrator.png"}
+        alt="ilustrator"
+        width={500}
+        height={500}
+        className="w-auto bg-slate-500 rounded-lg"
+      />
+    </div>
+  );
+}
