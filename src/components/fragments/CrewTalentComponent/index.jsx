e0786f7a05437b26d31102343ff63bc0a700e9ad
zidanabbas <zidan.abbas28@gmail.com>
2024-07-01 01:50:08 +0700

    feat : create crew and talent components

---
+++ src/components/fragments/CrewTalentComponent/index.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import CardTalent from "../Card/CardTalent";
+import ContainTitle from "@/components/ui/ContainTitle";
+import SectionContainerBody from "@/components/layouts/SectionPage/SectionContainerBody";
+import Button from "@/components/ui/Button";
+
+export default function CrewTalentComponent() {
+  return (
+    <div className="w-full bg-primary py-24">
+      <SectionContainerBody
+        classname={"bg-background-color flex flex-wrap justify-center"}
+      >
+        <ContainTitle
+          title={"Crew & Talent"}
+          classname={"text-primary w-full"}
+        />
+
+        <div className="container sm:flex sm:flex-wrap sm:gap-6 sm:justify-evenly bg-slate-100 py-5 rounded-lg max-h-[700px] overflow-scroll">
+          <CardTalent
+            src={"/images/crew1.png"}
+            alt="SUTRADARA DAN PENULIS CERITA"
+            role={"SUTRADARA DAN PENULIS CERITA"}
+            name="Sofwan Amin"
+            description={
+              " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, tempora ducimus reiciendis ea, saepe, dolor eligendi voluptatum deleniti modi omnis placeat laborum. Vel mollitia neque blanditiis, molestiae, architecto maiores, ipsa optio illum id enim soluta. Aspernatur voluptatibus repellendus rerum maiores mollitia nobis. Nostrum iste, ab necessitatibus dicta error quidem laudantium, veritatis facilis, vel totam ea repellat architecto ipsum temporibus aliquam molestiae magni sunt provident doloremque adipisci maxime. Repudiandae magni perspiciatis libero! Numquam voluptas eos doloremque perspiciatis minus aspernatur minima, quam tempora error, at non saepe sequi cumque velit cum esse necessitatibus suscipit labore! Inventore assumenda esse fuga voluptate minus, laudantium iure distinctio corrupti maxime consequatur suscipit soluta perferendis architecto modi commodi repellendus exercitationem corporis rerum eaque doloribus fugit nesciunt, similique adipisci consequuntur. Voluptates dolor quod sint inventore voluptas adipisci commodi vero maiores, blanditiis, ex ullam molestiae nihil porro provident similique nesciunt, cupiditate aut aliquam quaerat temporibus est? Adipisci, officia quam."
+            }
+          />
+          <CardTalent
+            src={"/images/crew1.png"}
+            alt="SUTRADARA DAN PENULIS CERITA"
+            role={"SUTRADARA DAN PENULIS CERITA"}
+            name="Sofwan Amin"
+            description={
+              " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, tempora ducimus reiciendis ea, saepe, dolor eligendi voluptatum deleniti modi omnis placeat laborum. Vel mollitia neque blanditiis, molestiae, architecto maiores, ipsa optio illum id enim soluta. Aspernatur voluptatibus repellendus rerum maiores mollitia nobis. Nostrum iste, ab necessitatibus dicta error quidem laudantium, veritatis facilis, vel totam ea repellat architecto ipsum temporibus aliquam molestiae magni sunt provident doloremque adipisci maxime. Repudiandae magni perspiciatis libero! Numquam voluptas eos doloremque perspiciatis minus aspernatur minima, quam tempora error, at non saepe sequi cumque velit cum esse necessitatibus suscipit labore! Inventore assumenda esse fuga voluptate minus, laudantium iure distinctio corrupti maxime consequatur suscipit soluta perferendis architecto modi commodi repellendus exercitationem corporis rerum eaque doloribus fugit nesciunt, similique adipisci consequuntur. Voluptates dolor quod sint inventore voluptas adipisci commodi vero maiores, blanditiis, ex ullam molestiae nihil porro provident similique nesciunt, cupiditate aut aliquam quaerat temporibus est? Adipisci, officia quam."
+            }
+          />
+          <CardTalent
+            src={"/images/crew1.png"}
+            alt="SUTRADARA DAN PENULIS CERITA"
+            role={"SUTRADARA DAN PENULIS CERITA"}
+            name="Sofwan Amin"
+            description={
+              " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, tempora ducimus reiciendis ea, saepe, dolor eligendi voluptatum deleniti modi omnis placeat laborum. Vel mollitia neque blanditiis, molestiae, architecto maiores, ipsa optio illum id enim soluta. Aspernatur voluptatibus repellendus rerum maiores mollitia nobis. Nostrum iste, ab necessitatibus dicta error quidem laudantium, veritatis facilis, vel totam ea repellat architecto ipsum temporibus aliquam molestiae magni sunt provident doloremque adipisci maxime. Repudiandae magni perspiciatis libero! Numquam voluptas eos doloremque perspiciatis minus aspernatur minima, quam tempora error, at non saepe sequi cumque velit cum esse necessitatibus suscipit labore! Inventore assumenda esse fuga voluptate minus, laudantium iure distinctio corrupti maxime consequatur suscipit soluta perferendis architecto modi commodi repellendus exercitationem corporis rerum eaque doloribus fugit nesciunt, similique adipisci consequuntur. Voluptates dolor quod sint inventore voluptas adipisci commodi vero maiores, blanditiis, ex ullam molestiae nihil porro provident similique nesciunt, cupiditate aut aliquam quaerat temporibus est? Adipisci, officia quam."
+            }
+          />
+          <CardTalent
+            src={"/images/crew1.png"}
+            alt="SUTRADARA DAN PENULIS CERITA"
+            role={"SUTRADARA DAN PENULIS CERITA"}
+            name="Sofwan Amin"
+            description={
+              " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, tempora ducimus reiciendis ea, saepe, dolor eligendi voluptatum deleniti modi omnis placeat laborum. Vel mollitia neque blanditiis, molestiae, architecto maiores, ipsa optio illum id enim soluta. Aspernatur voluptatibus repellendus rerum maiores mollitia nobis. Nostrum iste, ab necessitatibus dicta error quidem laudantium, veritatis facilis, vel totam ea repellat architecto ipsum temporibus aliquam molestiae magni sunt provident doloremque adipisci maxime. Repudiandae magni perspiciatis libero! Numquam voluptas eos doloremque perspiciatis minus aspernatur minima, quam tempora error, at non saepe sequi cumque velit cum esse necessitatibus suscipit labore! Inventore assumenda esse fuga voluptate minus, laudantium iure distinctio corrupti maxime consequatur suscipit soluta perferendis architecto modi commodi repellendus exercitationem corporis rerum eaque doloribus fugit nesciunt, similique adipisci consequuntur. Voluptates dolor quod sint inventore voluptas adipisci commodi vero maiores, blanditiis, ex ullam molestiae nihil porro provident similique nesciunt, cupiditate aut aliquam quaerat temporibus est? Adipisci, officia quam."
+            }
+          />
+          <CardTalent
+            src={"/images/crew1.png"}
+            alt="SUTRADARA DAN PENULIS CERITA"
+            role={"SUTRADARA DAN PENULIS CERITA"}
+            name="Sofwan Amin"
+            description={
+              " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, tempora ducimus reiciendis ea, saepe, dolor eligendi voluptatum deleniti modi omnis placeat laborum. Vel mollitia neque blanditiis, molestiae, architecto maiores, ipsa optio illum id enim soluta. Aspernatur voluptatibus repellendus rerum maiores mollitia nobis. Nostrum iste, ab necessitatibus dicta error quidem laudantium, veritatis facilis, vel totam ea repellat architecto ipsum temporibus aliquam molestiae magni sunt provident doloremque adipisci maxime. Repudiandae magni perspiciatis libero! Numquam voluptas eos doloremque perspiciatis minus aspernatur minima, quam tempora error, at non saepe sequi cumque velit cum esse necessitatibus suscipit labore! Inventore assumenda esse fuga voluptate minus, laudantium iure distinctio corrupti maxime consequatur suscipit soluta perferendis architecto modi commodi repellendus exercitationem corporis rerum eaque doloribus fugit nesciunt, similique adipisci consequuntur. Voluptates dolor quod sint inventore voluptas adipisci commodi vero maiores, blanditiis, ex ullam molestiae nihil porro provident similique nesciunt, cupiditate aut aliquam quaerat temporibus est? Adipisci, officia quam."
+            }
+          />
+        </div>
+        <Button href={"/crew"}>Read More</Button>
+      </SectionContainerBody>
+    </div>
+  );
+}
