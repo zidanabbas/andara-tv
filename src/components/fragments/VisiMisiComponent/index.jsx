cbac216ac4112541b2b5b6e07552a7a9e29446c7
zidanabbas <zidan.abbas28@gmail.com>
2024-07-01 01:53:55 +0700

    feat : adding a vision and mission componentadding a vision and mission component

---
+++ src/components/fragments/VisiMisiComponent/index.jsx
@@ -0,0 +1,12 @@
+import ContainTitle from "@/components/ui/ContainTitle";
+import React from "react";
+
+export default function VisiMisiComponent({ title, classname, children }) {
+  return (
+    <div className="container py-10">
+      <ContainTitle title={title} classname={classname} />
+
+      <div className="sm:flex justify-evenly gap-4 rounded-lg">{children}</div>
+    </div>
+  );
+}
