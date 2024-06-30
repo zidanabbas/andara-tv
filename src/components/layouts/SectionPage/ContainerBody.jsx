a6570e3d010dd9cf636a87ff42ee2fae1d1da6e7
zidanabbas <zidan.abbas28@gmail.com>
2024-07-01 01:51:03 +0700

    feat : create a wrapper to contain body

---
+++ src/components/layouts/SectionPage/ContainerBody.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ContainerBody({ children, classname }) {
+  return (
+    <section className={`w-full flex justify-center items-center ${classname}`}>
+      {children}
+    </section>
+  );
+}
