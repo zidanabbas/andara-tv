d4bd324bbed3fc6ad5f35c043dc723a1ed12400d
zidanabbas <zidan.abbas28@gmail.com>
2024-07-07 13:34:11 +0700

    style: update layout and styling of HomeView banner and text sections

--- src/view/HomeView.jsx
+++ src/view/HomeView.jsx
@@ -11,28 +11,25 @@ export default function HomeView() {
   return (
     <>
       <Banner classname={"h-screen flex justify-center items-center"}>
-        <div className="container">
-          <div className="sm:flex sm:flex-col lg:flex-row items-center gap-4">
-            <div className="w-full">
+        <div className="container mx-auto p-0">
+          <div className="flex flex-col lg:flex-row items-center gap-4">
+            <div className="w-full lg:w-2/3">
               <Image
-                src={"/images/ilustrator.png"}
+                src="/images/ilustrator.webp"
                 width={500}
                 height={500}
                 alt="ilustrator"
-                className="w-full h-full object-cover"
+                className="w-full h-auto object-cover"
                 priority
               />
             </div>
-            <div className="w-full">
-              <div className="text-center flex flex-col justify-center gap-2 md:gap-4 lg:gap-3">
-                <h1 className="text-2xl sm:text-2xl md:text-4xl lg:text-5xl font-bold">
-                  ANDARA TV{" "}
-                  <span className="text-primary inline-block">OFFICIAL</span>
-                </h1>
-                <h2 className="text-lg sm:text-xl md:text-2xl font-semibold">
-                  Berkolaborasi menggalih potensi
-                </h2>
-              </div>
+            <div className="w-full lg:w-1/3 gap-2">
+              <h1 className="flex justify-center text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold gap-2">
+                ANDARA TV <span className="text-primary">OFFICIAL</span>
+              </h1>
+              <h2 className="text-base sm:text-lg md:text-xl text-center font-normal">
+                Berkolaborasi menggalih potensi
+              </h2>
             </div>
           </div>
         </div>
