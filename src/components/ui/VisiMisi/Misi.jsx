1364c87a7a210bc274f86e5636bb0c503fd09e58
zidanabbas <zidan.abbas28@gmail.com>
2024-07-07 13:32:50 +0700

    style: update Misi component with hover effect and revised mission statements update Misi component with hover effect and revised mission statements

--- src/components/ui/VisiMisi/Misi.jsx
+++ src/components/ui/VisiMisi/Misi.jsx
@@ -3,25 +3,23 @@ import ContainTitle from "../ContainTitle"
 
 export default function Misi() {
   return (
-    <div className="sm:w-full md:w-1/2 px-4 py-2 mt-4 sm:mt-0 shadow-lg border">
+    <div className="sm:w-full md:w-1/2 px-4 py-2 mt-4 sm:mt-0 hover:border hover:border-primary hover:duration-500">
       <div className="sm:w-2/3 mx-auto">
         <ContainTitle
           title={"Misi"}
           classname={"text-primary font-bold uppercase text-center"}
         />
       </div>
-      <div className="text-slate-700 mt-4">
+      <div className="text-slate-700 mt-4 font-base">
         <p className="mb-2">
-          - Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-          Distinctio, iure!
+          - Meningkatkan keterampilan setiap anggota dalam berkesenian
         </p>
         <p className="mb-2">
-          - Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-          Distinctio, iure!
+          - Berkomitmen melestarikan seni dan budaya Indonesia
         </p>
         <p className="mb-2">
-          - Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-          Distinctio, iure!
+          - Bekerjasama saling berkolaborasi dengan organisasi sejenis untuk
+          melestarikan seni dan budaya
         </p>
       </div>
     </div>
