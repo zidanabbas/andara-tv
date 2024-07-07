6f45efd06e7158722860a1138c8003c79ca4fdf7
zidanabbas <zidan.abbas28@gmail.com>
2024-07-07 13:34:48 +0700

    style : style: update text styles and content in ProfileView component

--- src/view/ProfileView.jsx
+++ src/view/ProfileView.jsx
@@ -47,7 +47,7 @@ export default function ProfileView() {
               "text-center text-primary uppercase text-lg sm:text-xl md:text-2xl lg:text-4xl"
             }
           />
-          <p className="tracking-widest text-base font-medium px-2 text-center text-slate-500">
+          <p className="tracking-widest text-sm font-base px-2 text-pretty text-slate-500">
             Andara TV merupakan media kreatifitas dari potensi warga andara.
             Andara TV memberikan tontonan yang menggambarkan kehidupan
             masyarakat betawi pinggiran dan problematika masyrakatnya yang
@@ -57,13 +57,17 @@ export default function ProfileView() {
       </SectionContainerBody>
 
       <VisiMisiComponent title={"Tujuan"} classname={"text-primary "}>
-        <p className="text-slate-700 px-6 py-4">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum nemo
-          itaque alias, perspiciatis necessitatibus sapiente debitis voluptatum
-          voluptate doloribus cupiditate natus fugit error officia magni
-          voluptas vero ex eius ipsum consequatur quasi nostrum rem modi. Quam
-          culpa accusamus vero eum, sequi dignissimos hic nulla aperiam, natus
-          molestiae minima magnam. Tempora.
+        <p className="text-slate-500 text-pretty px-6 py-4 tracking-wider">
+          Andara TV bertujuan menjadi wadah kolaborasi untuk menggali potensi
+          seni dan budaya. Kami berkomitmen meningkatkan keterampilan setiap
+          anggota dalam berkesenian melalui pelatihan, workshop, dan mentorship,
+          serta menyediakan platform berbagi ide dan proyek seni. Selain itu,
+          kami berupaya melestarikan seni dan budaya Indonesia dengan mengadakan
+          acara budaya, mendokumentasikan seni tradisional, dan bekerja sama
+          dengan komunitas lokal. Kami juga aktif menjalin kerjasama dengan
+          organisasi sejenis untuk memperluas jaringan dan meningkatkan dampak
+          pelestarian seni dan budaya melalui proyek-proyek kolaboratif yang
+          dipromosikan secara internasional.
         </p>
       </VisiMisiComponent>
 
